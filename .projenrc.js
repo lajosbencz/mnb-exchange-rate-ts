548268947120100cee393de74fc400beaf83f0b2
lazos <lazos@lazos.me>
2025-11-22 07:08:44 +0100

    fixup! projen

--- .projenrc.js
+++ .projenrc.js
@@ -6,7 +6,7 @@ const project = new typescript.TypeScriptProject({
   name: 'mnb-exchange-rate',
   description: 'TypeScript client for Hungarian National Bank (MNB) exchange rate API',
   defaultReleaseBranch: 'master',
-  packageManager: NodePackageManager.YARN_CLASSIC,
+  packageManager: NodePackageManager.NPM,
   license: 'MIT',
   majorVersion: 0,
   
@@ -78,7 +78,6 @@ const project = new typescript.TypeScriptProject({
     '.idea/',
     '.vscode/',
     'node_modules/',
-    'yarn.lock',
     'build/',
   ],
 
